08fd909f8a1164f1820d5b6603850541d84bbb57
Kaua-Vidal <kauakauavidalpc@gmail.com>
2025-10-23 21:23:10 -0300

    installing the biome.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-import app from "./app.js";
+import app from './app.js';
 
 app.listen(3001, () => {
-    console.log("Server is running at port 3001")
-})
+	console.log('Server is running at port 3001');
+});
