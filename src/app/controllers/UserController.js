0f28e7ee49588c3f11ccc1c82b489c7c2bf03010
Kaua-Vidal <kauakauavidalpc@gmail.com>
2025-10-28 22:21:09 -0300

    validating and handling data errors

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -8,12 +8,36 @@
 
 import { v4 } from "uuid";
 import User from '../models/User.js'
+import * as Yup from 'yup';
 
 
 
 class UserController {
     async store(request, response) {
+        const schema = Yup.object({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password_hash: Yup.string().min(6).required(),
+            admin: Yup.boolean()
+        })
+        try {
+            schema.validateSync(request.body, {abortEarly: false, strict: true })
+        } catch (err) {
+            return response.status(400).json({error: err.errors});
+        }
+        
+
         const {name, email, password_hash, admin} = request.body
+
+        const existingUser = await User.findOne({
+            where: {
+                email,
+            },
+        });
+        if (existingUser) {
+            return response.status(400).json({ message: "Email already taken!" })
+        }
+
         const user = await User.create(
             {
                 id: v4(),
