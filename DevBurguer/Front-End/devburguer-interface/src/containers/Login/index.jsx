c984b7992a32c062ecbee822f3e64672b771c01d
Kaua-Vidal <kaua.vidal@dcx.ufpb.br>
2025-11-03 23:16:30 -0300

    Starting the login page

--- DevBurguer/Front-End/devburguer-interface/src/containers/Login/index.jsx
+++ DevBurguer/Front-End/devburguer-interface/src/containers/Login/index.jsx
@@ -1,11 +1,44 @@
-
+import {
+  Button,
+  Container,
+  Form,
+  InputContainer,
+  LeftContainer,
+  RightContainer,
+  Title
+} from './styles';
+import Logo from '../../assets/logo.svg';
 
 export function Login() {
+  return (
+    <Container>
+      <LeftContainer>
+        <img src={Logo} alt="logo-devburguer" />
+      </LeftContainer>
+      <RightContainer>
+        <Title>
+          Olá, seja bem vindo ao <span>Dev Burguer!</span>
+          <br />
+          Acesse com seu
+          <span> Login e senha.</span>
+        </Title>
 
+        <Form>
+          <InputContainer>
+            <label htmlFor="">Email</label>
+            <input type="email" />
+          </InputContainer>
 
-    return (
-        <div>
-            <h1>Login</h1>
-        </div>
-    )
+          <InputContainer>
+            <label htmlFor="">Senha</label>
+            <input type="password" />
+          </InputContainer>
+          <Button>Entrar</Button>
+        </Form>
+        <p>
+          Não possui conta? <a href="a">Clique aqui.</a>
+        </p>
+      </RightContainer>
+    </Container>
+  );
 }
